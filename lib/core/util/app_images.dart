6d756d309c235d81bd999edd337309c4dc7becb4
mona alashkar <monaashkar7@gmail.com>
2024-07-14 13:59:22 +0300

    add app image

---
+++ lib/core/util/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages{
+  static const String logo="assets/images/logo.svg";
+  static const String favorite="assets/images/favorite.svg";
+  static const String iconShoppingCart="assets/images/icon_shopping_cart.svg";
+  static const String star="assets/images/star.svg";
+  static const String iconSearch="assets/images/icon _search_.svg";
+}
